1c6844454e4702d2ecb05e8361a8dc248ccbc9b3
rifqi856 <rifqifahmi680@gmail.com>
2025-07-07 09:44:01 +0700

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 
 function App() {
 
+
   return (
     <div className="Parentbox">
-      <div className="foto">
-    <img src="minuman1.jpeg"  />
-      </div>
 
-      <div className="Deskripsi"></div>
     </div>
   )
+}
+
+function FotoProduct() {
+        <div className="foto">
+    <img src="sepatu2.jpg"  />
+      </div>
+}
+
+
+
+function ProductInfo () {
+    const category = "RUNNING";
+  const name = "White sports shoes";
+  const harga = "700.000";
+
+  return(
+  <div className="Deskripsi">
+  <p className='Cate'>{category}</p>
+  <h1 className="Title">{name}</h1>
+  <p className='Price'>IDR {harga}</p>
+  <p className="Info">
+    One of the most recognizable shoes in the AJ collection, the Air Jordan 3 Retro features lightweight, visible cushioning just like the original from '88. Signature details and materials celebrate the game-changing icon.
+  </p>
+</div>
+  )
+
+
+  
 }
 
 export default App
